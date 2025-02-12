3d3632fe09f89f998babe73c80bd366a128ddf01
JonathanCordero <Corderoj25@gmail.com>
2025-02-11 20:58:59 -0500

    Recreated project and connected to GitHub

---
+++ demo/src/main/java/com/example/GameDeal/GameDealApplication.java
@@ -0,0 +1,13 @@
+package com.example.GameDeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDealApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/GameDeal/GameDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GameDeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
